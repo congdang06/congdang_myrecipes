7ca2efd8ac381ec5e9118d109dfc32e202bd4cea
Cong Dang <congdang@gmail.com>
2018-07-02 20:04:26 -0700

    make app production to the heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://congdang-myrecipes.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://congdang-myrecipes.herokuapp.com', 'https://congdang-myrecipes.herokuapp.com']
 end
